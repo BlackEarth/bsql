28511a18e5bb5e31ef0f9817c8cfc56486ba679e
Sean Harrison <sah@blackearth.us>
2016-04-11 18:17:35 -0500

    release uploaded

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 
+config = {
+  "name": "bsql",
+  "version": "0.2.0",
+  "description": "Black Earth’s SQL Database Interface Library",
+  "url": "https://github.com/BlackEarth/bsql",
+  "author": "Sean Harrison",
+  "author_email": "sah@blackearthgroup.com",
+  "license": "LGPL 3.0",
+  "classifiers": [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    "Programming Language :: Python :: 3"
+  ],
+  "entry_points": {},
+  "install_requires": ["bl"],
+  "extras_require": {
+    "dev": [],
+    "test": []
+  },
+  "package_data": {
+    "": ["*.jar", "*.sql", "*.xml", "*.xsl", "*.md", "*.TEMPLATE"]
+  },
+  "data_files": [],
+  "scripts": []
+}
+
 import os, json
 from setuptools import setup, find_packages
 from codecs import open
@@ -7,9 +34,6 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(path, 'README.rst'), encoding='utf-8') as f:
     read_me = f.read()
-
-with open(os.path.join(path, 'setup.json'), encoding='utf-8') as f:
-    config = json.loads(f.read())
 
 setup(
     long_description=read_me,
