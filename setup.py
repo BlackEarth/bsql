6c147d46d181a38af4edd89008b66b5474e50109
Sean Harrison <sah@blackearth.us>
2017-02-09 17:46:04 -0600

    0.10.1 – improvements to the handling of strings and debug logging in database and model

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bsql",
-  "version": "0.10.0",
+  "version": "0.10.1",
   "description": "Black Earth’s SQL Database Interface Library",
   "url": "https://github.com/BlackEarth/bsql",
   "author": "Sean Harrison",
